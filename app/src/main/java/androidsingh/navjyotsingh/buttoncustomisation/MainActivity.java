8d8945db0e91dcf07b4f85f6e9d40be307245672
Navjyot singh <navjyot.marwah@gmail.com>
2018-12-18 02:47:54 +0530

    Initial commit

---
+++ app/src/main/java/androidsingh/navjyotsingh/buttoncustomisation/MainActivity.java
@@ -0,0 +1,18 @@
+package androidsingh.navjyotsingh.buttoncustomisation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+
+
+    }
+}
